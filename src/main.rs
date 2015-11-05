da8f5da5d66fa9ec6cdc1e41f7c2851b0abff282
Denis Kolodin <deniskolodin@gmail.com>
2015-11-05 15:31:07 +0300

    HTTP parser

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,43 @@ use std::net;
 use std::io::{Read, Write};
 
 fn main() {
-    const THREADS_NUM: usize = 10000;
+    const THREADS_NUM: usize = 1000;
 
+    println!("Starting...");
     let listener = Arc::new(Mutex::new(net::TcpListener::bind("127.0.0.1:10042").unwrap()));
     let counter = Arc::new((Mutex::new(0), Condvar::new()));
     let mut threads: Vec<_> = Vec::with_capacity(THREADS_NUM);
     for i in 0..THREADS_NUM {
+        println!("Generate thread {}", i);
         let listener = listener.clone();
         let counter = counter.clone();
         let t = thread::spawn(move || {
             let (mut stream, _) = listener.lock().unwrap().accept().unwrap();
-            let mut buf = Vec::new();
-            stream.read(&mut buf).unwrap();
-            let response = b"OK";
+            println!("Thread {} accepted", i);
+            let expected = [13, 10, 13, 10];
+            let mut eidx = 0;
+            let mut buf = [0; 10];
+            'outer: loop {
+                match stream.read(&mut buf) {
+                    Ok(v) => {
+                        for idx in 0..v {
+                            let c = buf[idx];
+                            if expected[eidx] == c {
+                                eidx += 1;
+                                if eidx >= expected.len() {
+                                    break 'outer;
+                                }
+                            } else {
+                                eidx = 0;
+                            }
+                        }
+                    },
+                    Err(_) => break,
+                }
+            }
+            println!("Request {:?}", buf);
+            let response = b"HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n";
+            println!("Thread {} write response", i);
             stream.write_all(response).unwrap();
 
             stream.shutdown(std::net::Shutdown::Both).unwrap();
